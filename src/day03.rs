d1125617ff1ef3e2d439919de40511f0752b72aa
amorphous1 <harrystew@gmail.com>
2025-12-04 17:43:36 +0000

    refactor day 3 solutions to avoid duplication

--- src/day03.rs
+++ src/day03.rs
@@ -1,37 +1,31 @@
 pub fn part1(banks: &str) -> usize {
-    banks.split('\n')
-        .map(|bank| { bank.chars().map(|ch| { ch.to_digit(10).unwrap() as usize }).collect() })
-        .map(|bank| { max_joltage(&bank) })
-        .sum()
+    total_joltage(banks, 2)
 }
 
 pub fn part2(banks: &str) -> usize {
-    banks.split('\n')
-        .map(|bank| { bank.chars().map(|ch| { ch.to_digit(10).unwrap() as usize }).collect() })
-        .map(|bank| { max_joltage12(&bank) })
-        .sum()
-}
-fn max_joltage(bank: &Vec<usize>) -> usize {
-    let (digit1, pos1) = find_max(bank, 0, bank.len()-2);
-    let (digit2, _pos2) = find_max(bank, pos1+1, bank.len()-1);
-    10 * digit1 + digit2
+    total_joltage(banks, 12)
 }
 
-fn max_joltage12(bank: &Vec<usize>) -> usize {
-    let (digit1, pos1) = find_max(bank, 0, bank.len()-12);
-    let (digit2, pos2) = find_max(bank, pos1+1, bank.len()-11);
-    let (digit3, pos3) = find_max(bank, pos2+1, bank.len()-10);
-    let (digit4, pos4) = find_max(bank, pos3+1, bank.len()-9);
-    let (digit5, pos5) = find_max(bank, pos4+1, bank.len()-8);
-    let (digit6, pos6) = find_max(bank, pos5+1, bank.len()-7);
-    let (digit7, pos7) = find_max(bank, pos6+1, bank.len()-6);
-    let (digit8, pos8) = find_max(bank, pos7+1, bank.len()-5);
-    let (digit9, pos9) = find_max(bank, pos8+1, bank.len()-4);
-    let (digit10, pos10) = find_max(bank, pos9+1, bank.len()-3);
-    let (digit11, pos11) = find_max(bank, pos10+1, bank.len()-2);
-    let (digit12, _pos12) = find_max(bank, pos11+1, bank.len()-1);
-    100000000000*digit1 + 10000000000*digit2 + 1000000000*digit3 + 100000000*digit4 + 10000000*digit5 + 1000000*digit6 + 100000*digit7 + 10000*digit8 + 1000*digit9 + 100*digit10 + 10*digit11 + digit12
+fn total_joltage(banks: &str, num_batteries: usize) -> usize {
+    banks.split('\n')
+        .map(|bank| { bank.chars().map(|ch| { ch.to_digit(10).unwrap() as usize }).collect() })
+        .map(|bank| { max_joltage(&bank, num_batteries) })
+        .sum()
+
 }
+
+fn max_joltage(bank: &Vec<usize>, num_batteries: usize) -> usize {
+    let mut result: usize = 0;
+    let mut from = 0;
+    for to in bank.len()-num_batteries..=bank.len()-1 {
+        result *= 10;
+        let (digit, pos) = find_max(bank, from, to);
+        result += digit;
+        from = pos + 1;
+    }
+    result
+}
+
 fn find_max(bank: &Vec<usize>, from: usize, to: usize) -> (usize, usize) {
     for digit in (1..=9).rev() {
         for pos in from..=to {
@@ -42,7 +36,6 @@ fn find_max(bank: &Vec<usize>, from: usize, to: usize) -> (usize, usize) {
     }
     (bank[from], from)
 }
-
 
 #[cfg(test)]
 mod tests {
